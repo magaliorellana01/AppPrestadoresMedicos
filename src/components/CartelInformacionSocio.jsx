59e4098abfb530520a040a83945150ac2d4e8b9e
Sol <snoguera@opengov.com>
2025-09-30 14:20:18 -0300

    remove patologia

--- src/components/CartelInformacionSocio.jsx
+++ src/components/CartelInformacionSocio.jsx
@@ -1,36 +1,33 @@
-import { Box, Typography, useTheme } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import CredencialIcon from "../icons/icono-credencial.png";
 
-
 export default function CartelInformacionSocio({ socio }) {
-  
-  const theme = useTheme();
   function calcularEdad(fechaNacimiento) {
-  if (!fechaNacimiento) return "—";
-  const hoy = new Date();
-  const nacimiento = new Date(fechaNacimiento);
-  let edad = hoy.getFullYear() - nacimiento.getFullYear();
-  const mes = hoy.getMonth() - nacimiento.getMonth();
+    if (!fechaNacimiento) return "—";
+    const hoy = new Date();
+    const nacimiento = new Date(fechaNacimiento);
+    let edad = hoy.getFullYear() - nacimiento.getFullYear();
+    const mes = hoy.getMonth() - nacimiento.getMonth();
 
-  if (mes < 0 || (mes === 0 && hoy.getDate() < nacimiento.getDate())) {
-    edad--;
-  }
+    if (mes < 0 || (mes === 0 && hoy.getDate() < nacimiento.getDate())) {
+      edad--;
+    }
 
-  return edad;
+    return edad;
   }
 
   return (
     <Box
       display="flex"
       flexDirection={{ xs: "column", sm: "column", md: "row" }}
-      alignItems={{ xs: "flex-start", sm: "flex-start" ,md: "center" }}
+      alignItems={{ xs: "flex-start", sm: "flex-start", md: "center" }}
       gap={2}
-      p={{ xs: 2, sm: 2 ,md: 3 }}
+      p={{ xs: 2, sm: 2, md: 3 }}
       sx={{
-        width: "100%",                 
-        maxWidth: 400,                 
+        width: "100%",
+        maxWidth: 400,
         border: "1px solid",
-        borderColor:"border.main",
+        borderColor: "border.main",
         borderRadius: 2,
         backgroundColor: "background.default",
         boxSizing: "border-box",
@@ -41,29 +38,38 @@ export default function CartelInformacionSocio({ socio }) {
         src={CredencialIcon}
         alt="icono credencial"
         sx={{
-          width: 100,  
+          width: 100,
           height: "auto",
           flexShrink: 0,
           alignSelf: { xs: "center", sm: "center", md: "flex-start" },
-          pt:{ md: 6 }
+          pt: { md: 6 },
         }}
       />
 
-      
       <Box
         display="grid"
         gap={0.5}
         sx={{
-          width: "100%",              
-          textAlign: { xs: "center",sm: "center", md: "left" },
+          width: "100%",
+          textAlign: { xs: "center", sm: "center", md: "left" },
         }}
       >
-        <Typography variant="body1" sx={{ color: "text.primary" }}><strong>Afiliado:</strong> {`${socio?.nombres ?? ""} ${socio?.apellidos ?? ""}`.trim() || "—"}</Typography>
-        <Typography variant="body1" sx={{ color: "text.primary" }}><strong>Edad:</strong> {calcularEdad(socio?.fecha_nacimiento)} años</Typography>
-        <Typography variant="body1" sx={{ color: "text.primary" }}><strong>Género:</strong> {socio?.genero ?? "—"}</Typography>
-        <Typography variant="body1" sx={{ color: "text.primary" }}><strong>Nro Afiliado:</strong> {socio?.nro_afiliado ?? "—"}</Typography>
-        <Typography variant="body1" sx={{ color: "text.primary" }}><strong>Miembro:</strong> {socio?.rol ?? "—"}</Typography>
-        <Typography variant="body1" sx={{ color: "text.primary" }}><strong>Patología:</strong> {socio?.historia_clinica?.patologia ?? "—"}</Typography>
+        <Typography variant="body1" sx={{ color: "text.primary" }}>
+          <strong>Afiliado:</strong>{" "}
+          {`${socio?.nombres ?? ""} ${socio?.apellidos ?? ""}`.trim() || "—"}
+        </Typography>
+        <Typography variant="body1" sx={{ color: "text.primary" }}>
+          <strong>Edad:</strong> {calcularEdad(socio?.fecha_nacimiento)} años
+        </Typography>
+        <Typography variant="body1" sx={{ color: "text.primary" }}>
+          <strong>Género:</strong> {socio?.genero ?? "—"}
+        </Typography>
+        <Typography variant="body1" sx={{ color: "text.primary" }}>
+          <strong>Nro Afiliado:</strong> {socio?.nro_afiliado ?? "—"}
+        </Typography>
+        <Typography variant="body1" sx={{ color: "text.primary" }}>
+          <strong>Miembro:</strong> {socio?.rol ?? "—"}
+        </Typography>
       </Box>
     </Box>
   );
