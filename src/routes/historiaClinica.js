f9a9278a2cd61166b301c588bffdaa984ea894ba
Matnahuel <matias.nahuel.carabajal.mc@gmail.com>
2025-09-30 02:46:20 -0300

    endpoint detalle de HC y gestion de notas

--- src/routes/historiaClinica.js
+++ src/routes/historiaClinica.js
@@ -1,18 +1,21 @@
 const express = require("express");
 const {
-  getHistoriasClinicas,
-  getHistoriaClinicaById,
-  createHistoriaClinica,
-  updateHistoriaClinica,
-  deleteHistoriaClinica,
+  getHistoriasClinicas,
+  getHistoriaClinicaById,
+  createHistoriaClinica,
+  updateHistoriaClinica,
+  deleteHistoriaClinica,
+  addNotaAHC,
 } = require("../controllers/historiaClinica.js");
 
 const router = express.Router();
 
 router.get("/", getHistoriasClinicas);
-router.get("/:id", getHistoriaClinicaById);
+router.get("/:id", getHistoriaClinicaById); 
 router.post("/", createHistoriaClinica);
 router.put("/:id", updateHistoriaClinica);
 router.delete("/:id", deleteHistoriaClinica);
+
+router.post("/:historiaClinicaId/notas", addNotaAHC);
 
 module.exports = router;
