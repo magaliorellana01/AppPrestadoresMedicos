ccc2b4db08bddb14708cbfa29e5f1299f1f2e317
Sol <snoguera@opengov.com>
2025-09-29 21:30:34 -0300

    .populate en las rutas

--- src/controllers/historiaClinica.js
+++ src/controllers/historiaClinica.js
@@ -1,28 +1,29 @@
 const HistoriaClinica = require("../models/historiaClinica");
 
 exports.getHistoriasClinicas = async (req, res) => {
-  const historiasClinicas = await HistoriaClinica.find();
+  const historiasClinicas = await HistoriaClinica.find().populate("socio");
   res.json({ message: "Historias Clinicas obtenidas correctamente", historiasClinicas });
 };
 
 exports.getHistoriaClinicaById = async (req, res) => {
-  const historiaClinica = await HistoriaClinica.findById(req.params.id);
+  const historiaClinica = await HistoriaClinica.findById(req.params.id).populate("socio");
   res.json({ message: "Historia Clinica obtenida correctamente", historiaClinica });
 };
 
 exports.createHistoriaClinica = async (req, res) => {
-  const historiaClinica = await HistoriaClinica.create(req.body);
+  const historiaClinica = await HistoriaClinica.create(req.body).populate("socio");
   res.json({ message: "Historia Clinica creada correctamente", historiaClinica });
 };
 
 exports.updateHistoriaClinica = async (req, res) => {
   const historiaClinica = await HistoriaClinica.findByIdAndUpdate(req.params.id, req.body, {
+    populate: "socio",
     new: true,
   });
   res.json({ message: "Historia Clinica actualizada correctamente", historiaClinica });
 };
 
 exports.deleteHistoriaClinica = async (req, res) => {
-  await HistoriaClinica.findByIdAndDelete(req.params.id);
+  await HistoriaClinica.findByIdAndDelete(req.params.id).populate("socio");
   res.json({ message: "Historia Clinica eliminada correctamente" });
 };
