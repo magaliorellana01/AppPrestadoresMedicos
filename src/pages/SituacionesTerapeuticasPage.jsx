33fe31f70a4308b583120209804823afbc23eeec
Sol <sol@MacBook-Pro-de-Sol.local>
2025-10-07 21:28:30 -0300

    nuevo modal

--- src/pages/SituacionesTerapeuticasPage.jsx
+++ src/pages/SituacionesTerapeuticasPage.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-import { Box, Button, TextField, Typography } from "@mui/material";
-import { Add } from "@mui/icons-material";
+import { Box, Button, Divider, IconButton, Modal, TextField, Typography } from "@mui/material";
+import { Add, Close } from "@mui/icons-material";
 
 const SituacionesTerapeuticasPage = ({ theme }) => {
   const [q, setQ] = useState("");
+  const [openModal, setOpenModal] = useState(false);
 
   return (
     <Box>
@@ -35,11 +36,44 @@ const SituacionesTerapeuticasPage = ({ theme }) => {
           }}
         />
 
-        <Button variant="contained" color="primary" sx={{ fontSize: "22px" }}>
+        <Button variant="contained" color="primary" sx={{ fontSize: "22px" }} onClick={() => setOpenModal(true)}>
           Agregar Situación Terapéutica
           <Add sx={{ ml: 1 }} />
         </Button>
       </Box>
+
+      <Modal open={openModal} onClose={() => setOpenModal(false)} sx={{ display: "flex", justifyContent: "center", alignItems: "center"}}>
+        
+        <Box sx={{ width: "500px", backgroundColor: "white", p: 4,  borderRadius: '22px', border: '1px solid #E5E7EB', width: { xs: "90%", md: "1008px" } }}>
+
+          <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+            <Typography color={theme.color.primary} fontSize="28px">Nueva Situación Terapéutica</Typography>
+            <IconButton onClick={() => setOpenModal(false)}>
+              <Close />
+            </IconButton>
+          </Box>
+
+          <Divider sx={{ my: 2 }} />
+
+          <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+            <TextField fullWidth size="small" label="DNI Afiliado" type="number" InputLabelProps={{ shrink: true }} aria-label="dni afiliado"/>
+            <TextField fullWidth size="small" label="Fecha de Alta" type="date" InputLabelProps={{ shrink: true }} aria-label="fecha de alta"/>
+            <TextField fullWidth size="small" label="Fecha de Finalización" type="date" InputLabelProps={{ shrink: true }} aria-label="fecha de finalización"/>
+            <TextField fullWidth size="small" label="Diagnóstico" InputLabelProps={{ shrink: true }} aria-label="diagnóstico"/>
+            <TextField fullWidth size="small" label="Tratamiento" InputLabelProps={{ shrink: true }} aria-label="tratamiento"/>
+          </Box>
+
+          <Box sx={{ display: "flex", justifyContent: "flex-end", mt: 2 }}>
+            <Button variant="contained" color="primary" sx={{ fontSize: "22px", width: "175px", borderRadius: "10px", color: 'white', backgroundColor: theme.color.secondary }}>
+              Cancelar
+            </Button>
+            <Button variant="contained" color="primary" sx={{ fontSize: "22px", width: "175px", ml: 2, borderRadius: "10px" }}>
+              Crear
+            </Button>
+          </Box>
+        </Box>
+
+      </Modal>
     </Box>
   );
 };
