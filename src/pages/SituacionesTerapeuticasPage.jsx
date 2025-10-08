5f1c24c1878cf7c42bd76e876b81ea13b5c978bf
Sol <sol@MacBook-Pro-de-Sol.local>
2025-10-07 21:05:57 -0300

    nueva pantalla

--- src/pages/SituacionesTerapeuticasPage.jsx
+++ src/pages/SituacionesTerapeuticasPage.jsx
@@ -1,11 +1,46 @@
-import React from "react";
-import { Typography } from "@mui/material";
+import React, { useState } from "react";
+import { Box, Button, TextField, Typography } from "@mui/material";
+import { Add } from "@mui/icons-material";
 
 const SituacionesTerapeuticasPage = ({ theme }) => {
+  const [q, setQ] = useState("");
+
   return (
-    <Typography variant="h4" color={theme.color.primary}>
-      Situaciones Terapéuticas
-    </Typography>
+    <Box>
+
+      <Typography variant="h4" color={theme.color.primary} mb={4}>
+        Situaciones Terapéuticas
+      </Typography>
+
+      <Box mb={2} display="flex" justifyContent="space-between">
+        <TextField
+          fullWidth
+          size="small"
+          value={q}
+          onChange={(e) => setQ(e.target.value)}
+          placeholder="Buscar por Nro Afiliado, Apellido o Teléfono"
+          InputProps={{
+            inputProps: { "aria-label": "buscar afiliado" },
+          }}
+          sx={{
+            maxWidth: 660,
+            backgroundColor: "white",
+            "& .MuiInputBase-input": {
+              py: 1,
+            },
+            "& .MuiOutlinedInput-root": {
+              borderRadius: 2,
+              fontSize: '22px',
+            },
+          }}
+        />
+
+        <Button variant="contained" color="primary" sx={{ fontSize: "22px" }}>
+          Agregar Situación Terapéutica
+          <Add sx={{ ml: 1 }} />
+        </Button>
+      </Box>
+    </Box>
   );
 };
 
