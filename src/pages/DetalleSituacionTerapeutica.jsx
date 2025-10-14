c799ac01a629f0b864302d55f3bb77d10ea2ff8b
IRojas99 <ivanemiliano.rojas@estudiantes.unahur.edu.ar>
2025-10-13 22:41:40 -0300

    Se agregó el cartel con la fecha de inicio y de fin

--- src/pages/DetalleSituacionTerapeutica.jsx
+++ src/pages/DetalleSituacionTerapeutica.jsx
@@ -18,7 +18,8 @@ export default function DetalleSituacionTerapeutica() {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
     const [diagnosticoEditable, setDiagnosticoEditable] = useState('');
-    const [tratamientoEditable, setTratamientoEditable] = useState(''); 
+    const [tratamientoEditable, setTratamientoEditable] = useState('');
+    const [fechaFinEditable, setFechaFinEditable] = useState("");
 
     const fetchSituacionTerapeutica = useCallback(async () => {
         setLoading(true);
@@ -40,16 +41,19 @@ export default function DetalleSituacionTerapeutica() {
     }, [fetchSituacionTerapeutica]);
 
     useEffect(() => {
-    if (situacion) {
-        
-        setDiagnosticoEditable(situacion.diagnostico || '');
-        
-       
-        setTratamientoEditable(situacion.tratamiento || '');
-        
-       
-    }
-}, [situacion]); 
+        if (situacion) {
+
+            setDiagnosticoEditable(situacion.diagnostico || '');
+
+
+            setTratamientoEditable(situacion.tratamiento || '');
+
+
+            situacion.fechaFin ? new Date(situacion.fechaFin).toISOString().slice(0, 10) : ''
+
+
+        }
+    }, [situacion]);
     if (loading) {
         return (
             <Box p={4} display="flex" justifyContent="center">
@@ -79,164 +83,217 @@ export default function DetalleSituacionTerapeutica() {
             <CartelInformacionSocio socio={situacion.socio} />
 
 
-    <Box
-    display="flex"
-    flexDirection={{ xs: "column", sm: "column", md: "row" }}
-    alignItems={{ xs: "center", sm: "center", md: "center" }}
-    gap={2}
-    p={{ xs: 2, sm: 2, md: 3 }}
-    sx={{
-        width: "100%",
-        maxWidth: 400,
-        border: "1px solid",
-        borderColor: "border.main",
-        borderRadius: 2,
-        backgroundColor: "background.default",
-        boxSizing: "border-box",
-    }}
->
-    
-    <Box
-        sx={{
-            display: "flex",
-            flexDirection: "column", 
-            alignItems: "center",    
-            flexShrink: 0 
-        }}
-    >
-     
-        <Box
-            component="img"
-            src={DiagnosticoIcon}
-            alt="icono diagnostico"
-            sx={{
-                width: 100,
-                height: "auto",
-                
-                flexShrink: 0,
-            }}
-        />
+            <Box
+                display="flex"
+                flexDirection={{ xs: "column", sm: "column", md: "row" }}
+                alignItems={{ xs: "center", sm: "center", md: "center" }}
+                gap={2}
+                p={{ xs: 2, sm: 2, md: 3 }}
+                sx={{
+                    width: "100%",
+                    maxWidth: 400,
+                    border: "1px solid",
+                    borderColor: "border.main",
+                    borderRadius: 2,
+                    backgroundColor: "background.default",
+                    boxSizing: "border-box",
+                }}
+            >
 
-      
-        <Typography
-            variant="subtitle2"
-            sx={{ 
-                color: "text.secondary", 
-                mt: 0.5, 
-                mb: 0.5 
-            }}
-        >
-            Diagnóstico
-        </Typography>
-    </Box>
+                <Box
+                    sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        flexShrink: 0
+                    }}
+                >
 
-   
-    <Box sx={{ flexGrow: 1, width: { xs: "100%", sm: "100%" } }}>
-        <TextField
-            fullWidth
-            multiline
-            variant="outlined"
-            minRows={6}
-            maxRows={6}
-            value={diagnosticoEditable}
-            onChange={(e) => setDiagnosticoEditable(e.target.value)}
-            inputProps={{ maxLength: 400 }}
-            sx={{
-                backgroundColor: "background.paper",
-                "& .MuiOutlinedInput-root": {
-                    "& fieldset": {
-                        borderColor: "divider",
-                    },
-                    "&:hover fieldset": {
-                        borderColor: "primary.main",
-                    },
-                    "&.Mui-focused fieldset": {
-                        borderColor: "primary.main",
-                    },
-                },
-            }}
-        />
-    </Box>
-</Box>
-<Box
-    display="flex"
-    flexDirection={{ xs: "column", sm: "column", md: "row" }}
-    alignItems={{ xs: "center", sm: "center", md: "center" }}
-    gap={2}
-    p={{ xs: 2, sm: 2, md: 3 }}
-    sx={{
-        width: "100%",
-        maxWidth: 400,
-        border: "1px solid",
-        borderColor: "border.main",
-        borderRadius: 2,
-        backgroundColor: "background.default",
-        boxSizing: "border-box",
-    }}
->
-    
-    <Box
-        sx={{
-            display: "flex",
-            flexDirection: "column", 
-            alignItems: "center",    
-            flexShrink: 0 
-        }}
-    >
-     
-        <Box
-            component="img"
-            src={TratamientoIcon}
-            alt="icono diagnostico"
-            sx={{
-                width: 100,
-                height: "auto",
-                
-                flexShrink: 0,
-            }}
-        />
+                    <Box
+                        component="img"
+                        src={DiagnosticoIcon}
+                        alt="icono diagnostico"
+                        sx={{
+                            width: 100,
+                            height: "auto",
 
-      
-        <Typography
-            variant="subtitle2"
-            sx={{ 
-                color: "text.secondary", 
-                mt: 0.5, 
-                mb: 0.5 
-            }}
-        >
-            Tratamiento
-        </Typography>
-    </Box>
+                            flexShrink: 0,
+                        }}
+                    />
 
-   
-    <Box sx={{ flexGrow: 1, width: { xs: "100%", sm: "100%" } }}>
-        <TextField
-            fullWidth
-            multiline
-            variant="outlined"
-            minRows={6}
-            maxRows={6}
-            value={tratamientoEditable}
-            onChange={(e) => setTratamientoEditable(e.target.value)}
-            inputProps={{ maxLength: 400 }}
-            sx={{
-                backgroundColor: "background.paper",
-                "& .MuiOutlinedInput-root": {
-                    "& fieldset": {
-                        borderColor: "divider",
-                    },
-                    "&:hover fieldset": {
-                        borderColor: "primary.main",
-                    },
-                    "&.Mui-focused fieldset": {
-                        borderColor: "primary.main",
-                    },
-                },
-            }}
-        />
-    </Box>
-</Box>
+
+                    <Typography
+                        variant="subtitle2"
+                        sx={{
+                            color: "text.secondary",
+                            mt: 0.5,
+                            mb: 0.5
+                        }}
+                    >
+                        Diagnóstico
+                    </Typography>
+                </Box>
+
+
+                <Box sx={{ flexGrow: 1, width: { xs: "100%", sm: "100%" } }}>
+                    <TextField
+                        fullWidth
+                        multiline
+                        variant="outlined"
+                        minRows={6}
+                        maxRows={6}
+                        value={diagnosticoEditable}
+                        onChange={(e) => setDiagnosticoEditable(e.target.value)}
+                        inputProps={{ maxLength: 400 }}
+                        sx={{
+                            backgroundColor: "background.paper",
+                            "& .MuiOutlinedInput-root": {
+                                "& fieldset": {
+                                    borderColor: "divider",
+                                },
+                                "&:hover fieldset": {
+                                    borderColor: "primary.main",
+                                },
+                                "&.Mui-focused fieldset": {
+                                    borderColor: "primary.main",
+                                },
+                            },
+                        }}
+                    />
+                </Box>
+            </Box>
+            <Box
+                display="flex"
+                flexDirection={{ xs: "column", sm: "column", md: "row" }}
+                alignItems={{ xs: "center", sm: "center", md: "center" }}
+                gap={2}
+                p={{ xs: 2, sm: 2, md: 3 }}
+                sx={{
+                    width: "100%",
+                    maxWidth: 400,
+                    border: "1px solid",
+                    borderColor: "border.main",
+                    borderRadius: 2,
+                    backgroundColor: "background.default",
+                    boxSizing: "border-box",
+                }}
+            >
+
+                <Box
+                    sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        flexShrink: 0
+                    }}
+                >
+
+                    <Box
+                        component="img"
+                        src={TratamientoIcon}
+                        alt="icono diagnostico"
+                        sx={{
+                            width: 100,
+                            height: "auto",
+
+                            flexShrink: 0,
+                        }}
+                    />
+
+
+                    <Typography
+                        variant="subtitle2"
+                        sx={{
+                            color: "text.secondary",
+                            mt: 0.5,
+                            mb: 0.5
+                        }}
+                    >
+                        Tratamiento
+                    </Typography>
+                </Box>
+
+
+                <Box sx={{ flexGrow: 1, width: { xs: "100%", sm: "100%" } }}>
+                    <TextField
+                        fullWidth
+                        multiline
+                        variant="outlined"
+                        minRows={6}
+                        maxRows={6}
+                        value={tratamientoEditable}
+                        onChange={(e) => setTratamientoEditable(e.target.value)}
+                        inputProps={{ maxLength: 400 }}
+                        sx={{
+                            backgroundColor: "background.paper",
+                            "& .MuiOutlinedInput-root": {
+                                "& fieldset": {
+                                    borderColor: "divider",
+                                },
+                                "&:hover fieldset": {
+                                    borderColor: "primary.main",
+                                },
+                                "&.Mui-focused fieldset": {
+                                    borderColor: "primary.main",
+                                },
+                            },
+                        }}
+                    />
+                </Box>
+            </Box>
+            <Box
+                display="flex"
+                flexDirection="column"
+                gap={2}
+                p={{ xs: 2, sm: 2, md: 3 }}
+                sx={{
+                    width: "100%",
+                    maxWidth: 400,
+                    border: "1px solid",
+                    borderColor: "border.main",
+                    borderRadius: 2,
+                    backgroundColor: "background.default",
+                    boxSizing: "border-box",
+                }}
+            >
+                <Box display="flex" alignItems="center" justifyContent="space-between" width="100%">
+                    <Typography
+                        variant="subtitle2"
+                        sx={{ color: "text.secondary", minWidth: 150 }}
+                    >
+                        Fecha de Inicio
+                    </Typography>
+                    <TextField
+                        type="date"
+                        variant="outlined"
+                        size="small"
+                        value={
+                            situacion.fechaInicio
+                                ? new Date(situacion.fechaInicio).toISOString().slice(0, 10)
+                                : ""
+                        }
+                        InputLabelProps={{ shrink: true }}
+                        sx={{ flexGrow: 1 }}
+                    />
+                </Box>
+                <Box display="flex" alignItems="center" justifyContent="space-between" width="100%">
+                    <Typography
+                        variant="subtitle2"
+                        sx={{ color: "text.secondary", minWidth: 150 }}
+                    >
+                        Fecha de Fin
+                    </Typography>
+                    <TextField
+                        type="date"
+                        variant="outlined"
+                        size="small"
+                        value={fechaFinEditable}
+                        onChange={(e) => setFechaFinEditable(e.target.value)}
+                        InputLabelProps={{ shrink: true }}
+                        sx={{ flexGrow: 1 }}
+                    />
+                </Box>
+            </Box>
         </Box>
 
     )
